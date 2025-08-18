6435d5b211d7dc3ba97f5cf745610163481cc200
jory-drones <jory@securitydrones.info>
2025-08-17 20:08:38 -0700

    Update postcss.config.js

--- securitydrones-info-site/postcss.config.js
+++ securitydrones-info-site/postcss.config.js
@@ -1,6 +1,7 @@
+// Tailwind v4 PostCSS config
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+  plugins: [tailwindcss(), autoprefixer()]
+}
