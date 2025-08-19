5fa597c3adebd99e94f53214eb5a5993cdb0e95d
jory-drones <jory@securitydrones.info>
2025-08-18 21:52:35 -0700

    Update App.jsx

--- securitydrones-info-site/src/App.jsx
+++ securitydrones-info-site/src/App.jsx
@@ -1,4 +1,5 @@
 // website/securitydrones-info-site/src/App.jsx
+// /src/App.jsx
 import React from "react";
 import Navbar from "./components/Navbar.jsx";
 import HeroSplit from "./components/HeroSplit.jsx";
@@ -6,21 +7,19 @@ import BeeSection from "./components/BeeSection.jsx";
 import HiveSection from "./components/HiveSection.jsx";
 import SystemSection from "./components/SystemSection.jsx";
 import Features from "./components/Features.jsx";
-import CTABanner from "./components/CTABanner.jsx";   
-import ROICalculator from "./components/ROICalculator.jsx";
+import CTABanner from "./components/CTABanner.jsx";
 import AboutContacts from "./components/AboutContacts.jsx";
 
 export default function App() {
   return (
-    <main>
+    <main className="bg-primary text-white">
       <Navbar />
       <HeroSplit />
       <BeeSection />
       <HiveSection />
       <SystemSection />
       <Features />
-      <CTABanner />            
-      <ROICalculator />
+      <CTABanner />
       <AboutContacts />
     </main>
   );
