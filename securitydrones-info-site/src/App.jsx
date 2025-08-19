72d5b42dccdd22764cf940159a7cd4fc0aa503f6
jory-drones <jory@securitydrones.info>
2025-08-18 21:37:03 -0700

    Update App.jsx

--- securitydrones-info-site/src/App.jsx
+++ securitydrones-info-site/src/App.jsx
@@ -1,29 +1,27 @@
 // website/securitydrones-info-site/src/App.jsx
 import React from "react";
 import Navbar from "./components/Navbar.jsx";
-import HeroSplit from "./components/HeroSplit";
-import BeeSection from "./components/BeeSection";
-import HiveSection from "./components/HiveSection";
-import SystemSection from "./components/SystemSection";
-import Capabilities from "./components/Capabilities";
-import ROICalculator from "./components/ROICalculator";
-import CalendlyEmbed from "./components/CalendlyEmbed";
-
+import HeroSplit from "./components/HeroSplit.jsx";
+import BeeSection from "./components/BeeSection.jsx";
+import HiveSection from "./components/HiveSection.jsx";
+import SystemSection from "./components/SystemSection.jsx";
+import Features from "./components/Features.jsx";
+import CTABanner from "./components/CTABanner.jsx";   
+import ROICalculator from "./components/ROICalculator.jsx";
+import AboutContacts from "./components/AboutContacts.jsx";
 
 export default function App() {
   return (
-    <div id="top" className="bg-primary text-white">
+    <main>
       <Navbar />
-      <main className="pt-14 sm:pt-16">
-        <HeroSplit />
-        <BeeSection />
-        <HiveSection />
-        <SystemSection />
-        <Capabilities />
-        <CTABanner />
-        <CalendlyEmbed />
-        <AboutContact />
-      </main>
-    </div>
+      <HeroSplit />
+      <BeeSection />
+      <HiveSection />
+      <SystemSection />
+      <Features />
+      <CTABanner />            
+      <ROICalculator />
+      <AboutContacts />
+    </main>
   );
 }
