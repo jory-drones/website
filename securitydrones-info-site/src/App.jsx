7b769038f066cbb5be17c34bbe6747f82e267b07
jory-drones <jory@securitydrones.info>
2025-08-20 20:23:38 -0700

    Update App.jsx

--- securitydrones-info-site/src/App.jsx
+++ securitydrones-info-site/src/App.jsx
@@ -1,6 +1,7 @@
 // website/securitydrones-info-site/src/App.jsx
 // /src/App.jsx
 import React from "react";
+
 import Navbar from "./components/Navbar.jsx";
 import HeroSplit from "./components/HeroSplit.jsx";
 import BeeSection from "./components/BeeSection.jsx";
@@ -8,13 +9,13 @@ import HiveSection from "./components/HiveSection.jsx";
 import SystemSection from "./components/SystemSection.jsx";
 import Features from "./components/Features.jsx";
 import Integrations from "./components/Integrations.jsx";
-import DemoCTA from "./components/DemoCTA.jsx";
+import DemoCTA from "./components/DemoCTA.jsx";    
 import CalendlyEmbed from "./components/CalendlyEmbed.jsx";
-import Footer from "./components/Footer.jsx";
+import Footer from "./components/Footer.jsx";  
 
 export default function App() {
   return (
-    <main>
+    <main className="bg-primary text-white font-body">
       <Navbar />
       <HeroSplit />
       <BeeSection />
@@ -24,7 +25,6 @@ export default function App() {
       <Integrations />
       <DemoCTA />
       <CalendlyEmbed />
-      <AboutContacts />
       <Footer />
     </main>
   );
