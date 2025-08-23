cf83906c92edc3a76b90dbf499c1708bc547a4e4
jory-drones <jory@securitydrones.info>
2025-08-23 14:20:58 -0700

    Update App.jsx

--- securitydrones-info-site/src/App.jsx
+++ securitydrones-info-site/src/App.jsx
@@ -1,30 +1,34 @@
 // website/securitydrones-info-site/src/App.jsx
 // /src/App.jsx
 import React from "react";
-
 import Navbar from "./components/Navbar.jsx";
 import HeroSplit from "./components/HeroSplit.jsx";
-import BeeSection from "./components/BeeSection.jsx";
-import HiveSection from "./components/HiveSection.jsx";
 import SystemSection from "./components/SystemSection.jsx";
+import HiveSection from "./components/HiveSection.jsx";
+import BeeSection from "./components/BeeSection.jsx";
 import Features from "./components/Features.jsx";
 import Integrations from "./components/Integrations.jsx";
-import DemoCTA from "./components/DemoCTA.jsx";    
-import CalendlyEmbed from "./components/CalendlyEmbed.jsx";
-import Footer from "./components/Footer.jsx";  
+import ROICalculator from "./components/ROICalculator.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
-    <main className="bg-primary text-white font-body">
+    <main className="font-body bg-primary text-white">
       <Navbar />
       <HeroSplit />
-      <BeeSection />
-      <HiveSection />
-      <SystemSection />
+
+      {/* ORDER: The System, The Hive, The Drone */}
+      <section id="services">
+        <SystemSection />
+        <HiveSection />
+        <BeeSection />
+      </section>
+
       <Features />
       <Integrations />
-      <DemoCTA />
-      <CalendlyEmbed />
+      <section id="book">
+        <ROICalculator />
+      </section>
       <Footer />
     </main>
   );
