e4f47ca246593f4232c14e05669036b20d4a9e6c
jory-drones <jory@securitydrones.info>
2025-08-23 14:51:12 -0700

    Update App.jsx

--- securitydrones-info-site/src/App.jsx
+++ securitydrones-info-site/src/App.jsx
@@ -6,29 +6,22 @@ import HeroSplit from "./components/HeroSplit.jsx";
 import SystemSection from "./components/SystemSection.jsx";
 import HiveSection from "./components/HiveSection.jsx";
 import BeeSection from "./components/BeeSection.jsx";
-import Features from "./components/Features.jsx";
+import Capabilities from "./components/Capabilities.jsx";
 import Integrations from "./components/Integrations.jsx";
-import ROICalculator from "./components/ROICalculator.jsx";
+import DemoCTA from "./components/DemoCTA.jsx";
 import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
-    <main className="font-body bg-primary text-white">
+    <main>
       <Navbar />
       <HeroSplit />
-
-      {/* ORDER: The System, The Hive, The Drone */}
-      <section id="services">
-        <SystemSection />
-        <HiveSection />
-        <BeeSection />
-      </section>
-
-      <Features />
+      <section id="system"><SystemSection /></section>
+      <section id="hive"><HiveSection /></section>
+      <section id="bee"><BeeSection /></section>
+      <Capabilities />
       <Integrations />
-      <section id="book">
-        <ROICalculator />
-      </section>
+      <DemoCTA />
       <Footer />
     </main>
   );
