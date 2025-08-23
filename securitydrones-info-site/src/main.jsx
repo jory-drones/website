38fab98d28ff114274a3f9434a2eb8ef14ddf3c3
jory-drones <jory@securitydrones.info>
2025-08-23 15:45:55 -0700

    Update main.jsx

--- securitydrones-info-site/src/main.jsx
+++ securitydrones-info-site/src/main.jsx
@@ -1,7 +1,6 @@
-// /src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // Tailwind v4 CSS with @theme tokens
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(<App />);
